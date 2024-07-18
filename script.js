7838b3359e8d92fb9a1d37d2316963a50da0a3b9
Favinzy <mmesomaoguezu@gmail.com>
2024-07-18 11:53:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// let media = document.querySelector(".media");
+// function slide() {
+//   media.classList.toggle("slideout");
+// }
+
+let media = document.querySelector( '.media')
+
+const slide = () =>{
+  media.classList.toggle("slideout")
+}
